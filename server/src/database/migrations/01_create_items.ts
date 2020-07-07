76b967257339d475011a63955fd545420ebf2343
NAGAMINE DENIS HIDEO <hiden@MacBook-Pro-de-NAGAMINE.local>
2020-07-07 19:59:21 -0300

    App

---
+++ server/src/database/migrations/01_create_items.ts
@@ -0,0 +1,16 @@
+import Knex from 'knex';
+ 
+
+export async function up(knex: Knex) {
+    knex.
+    schema.createTable('items', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+        
+    }).then();
+}
+
+export async function down(knex: Knex) {
+    knex.schema.dropTable('items')
+}
